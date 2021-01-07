324fee3e1578c828f180b351842b71edab84562b
Andre Oliveira <desmonplays@gmail.com>
2021-01-06 23:04:38 -0300

    feat: requiring sinatra active record

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require 'dotenv/load'
 require 'json'
+require 'sinatra'
 require 'sinatra/base'
+require 'sinatra/activerecord'
+require './config/database'
 
 class ApplicationController < Sinatra::Base
 end
